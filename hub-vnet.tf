57a00c9bdf44ed50c8b003bcec20836494c80290
Markus Kraus <markus.kraus@gmail.com>
2024-02-08 20:34:03 +0100

    terraform_deprecated_interpolation

--- hub-vnet.tf
+++ hub-vnet.tf
@@ -18,6 +18,6 @@ resource "azurerm_subnet" "subnet-hub" {
   name                 = var.hub-subnet_names[count.index]
   virtual_network_name = azurerm_virtual_network.vnet-hub.name
   resource_group_name  = azurerm_resource_group.rg-hub.name
-  address_prefixes     = ["${var.hub-subnet_prefixes[count.index]}"]
+  address_prefixes     = [ var.hub-subnet_prefixes[count.index] ]
   count                = length(var.hub-subnet_names)
 }
