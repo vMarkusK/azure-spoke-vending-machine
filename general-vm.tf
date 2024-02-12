daa2e313898aa55823ecc0f2848be3d0e8dcab21
Markus Kraus <markus.kraus@gmail.com>
2024-02-12 22:14:25 +0100

    create ProximityPlacementGroup for Az1

--- general-vm.tf
+++ general-vm.tf
@@ -2,3 +2,12 @@ resource "azurerm_resource_group" "compute_rg" {
   name     = var.compute_rg_name
   location = var.location
 }
+
+resource "azurerm_proximity_placement_group" "placement_group_az1" {
+  name                = "ProximityPlacementGroupAz1"
+  location            = var.location
+  resource_group_name = azurerm_resource_group.compute_rg.name
+  zone                = 1
+  allowed_vm_sizes    = [ var.vm_size ]
+  tags                = var.tags
+}
