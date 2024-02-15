8fffddf379fc79d064dacfb028347724c3391e24
Markus Kraus <markus.kraus@gmail.com>
2024-02-15 22:40:21 +0100

    random provicer

--- main.tf
+++ main.tf
@@ -4,6 +4,10 @@ terraform {
       source  = "hashicorp/azurerm"
       version = ">= 3.86.0"
     }
+    random = {
+      source  = "hashicorp/random"
+      version = ">= 3.6.0"
+    }
   }
   
   required_version = ">= 1.6.6"
